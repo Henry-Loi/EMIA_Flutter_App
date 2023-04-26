119ee059cfaa32c4edad01dd98b0b462bc4df6ad
MarcusCCH <marcuscch12@gmail.com>
2023-04-26 20:05:45 +0800

    add dropdown menu

--- lib/page/session/session_init.dart
+++ lib/page/session/session_init.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
+class DropDown extends StatefulWidget {
+  DropDown({super.key, required this.list});
+  late List<String> list;
+  @override
+  State<DropDown> createState() => _DropDown();
+}
+
+class _DropDown extends State<DropDown> {
+  late String? _dropDownValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _dropDownValue = widget.list[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: _dropDownValue,
+      onChanged: (String? newValue) {
+        setState(() {
+          _dropDownValue = newValue;
+        });
+      },
+      items: widget.list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
 class InitSession extends StatelessWidget {
   const InitSession({super.key});
   @override
@@ -9,12 +44,22 @@ class InitSession extends StatelessWidget {
         child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
           const Text(
             'Choose the duration of your focus session',
-            style: const TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
           ),
+          DropDown(list: <String>[
+            "15 minutes",
+            "30 minutes",
+            "45 minutes",
+            "60 minutes",
+          ]),
           const Text("Choose your pet"),
+          DropDown(list: <String>[
+            "cat",
+            "dog",
+            "bird",
+            "fish",
+            "hamster",
+            "rabbit"
+          ]),
         ]));
   }
 }
